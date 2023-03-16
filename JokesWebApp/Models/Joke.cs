a368a4e0ece8f4fd4c38f44d1341b9a7c14902c5
Tyler Voss <tylervoss101@gmail.com>
2023-03-16 10:37:18 -0400

    Add project files.

---
+++ JokesWebApp/Models/Joke.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace JokesWebApp.Models
+{
+    public class Joke
+    {
+        public int id { get; set; }
+        public string JokeQuestion { get; set; }
+        public string JokeAnswer { get; set; }
+
+        public Joke()
+        {
+        }
+    }
+}
